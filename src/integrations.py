31ffd84aa79400b7a2a9d2af99e8e9e562da698d
nedogeek <urunbaev.timur@gmail.com>
2024-05-08 13:59:27 +0500

    feat: integrations

---
+++ src/integrations.py
@@ -0,0 +1,62 @@
+class Web:
+    def __init__(self):
+        """
+        This class is used for websearch related integrations.
+        """
+        self.result =  None
+        self.name = "Web"
+        self.icon = "external-link-symbolic"
+
+class Calculator:
+    def __init__(self):
+        """
+        This class is used for calculator related integrations.
+        """
+        self.result = None
+        self.name = "Calculator"
+        self.icon = "copy-symbolic"
+
+class Calendar:
+    def __init__(self):
+        """
+        This class is used for calendar related integrations.
+        """
+        self.result = None
+        self.name = "Calendar"
+        self.icon = "external-link-symbolic"
+
+class Weather:
+    def __init__(self):
+        self.result = None
+        self.name = "Weather"
+        self.icon = ""
+
+class Clock:
+    def __init__(self):
+        self.result = None
+        self.name = "Clock"
+        self.icon = ""
+
+class Contacts:
+    def __init__(self):
+        self.result = None
+        self.name = "Contacts"
+        self.icon = ""
+
+class Files:
+    def __init__(self):
+        self.result = None
+        self.name = "Files"
+        self.icon = ""
+
+    def search(self, pattern):
+        pass
+
+class Settings:
+    def __init__(self):
+        self.result = None
+        self.name = "Settings"
+        self.icon = ""
+
+    def search(self):
+        pass
